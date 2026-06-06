2215869d7cdbfdc3bdca7a5944bd80f73d99e54a
zaro5 <zahwaasmoro@gmail.com>
2026-06-07 03:21:56 +0700

    Perbaikan :)

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,10 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
-    imageSizes: [16, 32, 48, 64, 96, 128, 220, 256, 320, 384, 440, 514],
+    imageSizes: [16, 32, 48, 64, 96, 128, 220, 256, 320],
     formats: ["image/avif", "image/webp"],
-  }
+  },
+  turbopack: {},
 }
 
 export default nextConfig
