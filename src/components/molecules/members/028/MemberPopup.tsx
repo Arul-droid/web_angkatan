2687cbd0d527d597d4c04a1e3c423cbea2249257
Evandra Raditya <evandraraditya049@gmail.com>
2026-06-07 15:58:29 +0700

    fix: bug

--- src/components/molecules/members/028/MemberPopup.tsx
+++ src/components/molecules/members/028/MemberPopup.tsx
@@ -9,7 +9,7 @@ import Instagram from '@/components/atoms/button/InstagramButtonLink'
 import LinkedInButtonLink from '@/components/atoms/button/LinkedInButtonLink'
 import SpotifyEmbed from '@/components/molecules/SpotifyEmbed'
 
-import ProfileImage from './image.png'
+import ProfileImage from './foto webang 2.png'
 
 type MemberPopupProps = {
   isOpen: boolean
